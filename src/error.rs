a99a539ef086ba88348002ea6635cc43e4f0b9da
Eric Holk <eric.holk@gmail.com>
2022-08-20 18:28:58 -0700

    Make Error be Sync too

--- src/error.rs
+++ src/error.rs
@@ -4,9 +4,11 @@ use zip::result::ZipError;
 
 use crate::db_entries::Tmpl;
 
-// Make sure `Error` is `Send`
+// Make sure `Error` is `Send` and `Sync`
 const fn _assert_send<T: Send>() {}
+const fn _assert_sync<T: Sync>() {}
 const _: () = _assert_send::<Error>();
+const _: () = _assert_sync::<Error>();
 
 #[derive(thiserror::Error, Debug)]
 #[non_exhaustive]
@@ -18,7 +20,7 @@ pub enum Error {
     /// the underlying library in the future if needed without breaking
     /// client code.
     #[error(transparent)]
-    Database(Box<dyn std::error::Error + Send>),
+    Database(Box<dyn std::error::Error + Send + Sync>),
     /// Indicates an error happened with the JSON parser
     ///
     /// Currently the argument is a `serde_json::Error`, but it is
@@ -26,7 +28,7 @@ pub enum Error {
     /// the underlying library in the future if needed without breaking
     /// client code.
     #[error(transparent)]
-    JsonParser(Box<dyn std::error::Error + Send>),
+    JsonParser(Box<dyn std::error::Error + Send + Sync>),
     #[error("Could not compute required fields for this template; please check the formatting of \"qfmt\": {0:?}")]
     TemplateFormat(Tmpl),
     #[error("number of model field ({0}) does not match number of fields ({1})")]
@@ -42,7 +44,7 @@ pub enum Error {
     /// the underlying library in the future if needed without breaking
     /// client code.
     #[error(transparent)]
-    Template(#[from] Box<dyn std::error::Error + Send>),
+    Template(#[from] Box<dyn std::error::Error + Send + Sync>),
     #[error(transparent)]
     SystemTime(#[from] SystemTimeError),
     /// Indicates an error with zip file handling
@@ -52,7 +54,7 @@ pub enum Error {
     /// the underlying library in the future if needed without breaking
     /// client code.
     #[error(transparent)]
-    Zip(Box<dyn std::error::Error + Send>),
+    Zip(Box<dyn std::error::Error + Send + Sync>),
 }
 
 impl From<Infallible> for Error {
