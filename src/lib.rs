6ddffe94be05e34c15053eb130c837c304b43b33
Yannick Funk <yannickfunk@yahoo.de>
2022-09-04 18:56:51 +0200

    Prepare release

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //!
 //! To use genanki-rs, add the following to your `Cargo.toml`
 //! ```toml
-//! genanki-rs = "0.2"
+//! genanki-rs = "0.3"
 //! ```
 //! ## Minimal Example
 //! The following example creates a simple deck, containing 2 question-answer flashcards:
